6a2255c90c3b329b15fd9ffcee535f5f97895a08
NipunaDassanayake <nipuna920@gmail.com>
2025-12-10 16:27:23 +0530

    feat: update AiTestController with REST annotations for improved API structure

--- coursecontent/src/main/java/com/silverline/task/coursecontent/controller/AiTestController.java
+++ coursecontent/src/main/java/com/silverline/task/coursecontent/controller/AiTestController.java
@@ -2,7 +2,9 @@ package com.silverline.task.coursecontent.controller;
 
 import com.silverline.task.coursecontent.service.AiSummarizationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
