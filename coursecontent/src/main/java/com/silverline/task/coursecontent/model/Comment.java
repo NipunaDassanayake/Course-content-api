38525b8926feed92b77a903ca0fc0d7d74337805
NipunaDassanayake <nipuna920@gmail.com>
2025-12-11 15:25:42 +0530

    created a comment model

---
+++ coursecontent/src/main/java/com/silverline/task/coursecontent/model/Comment.java
@@ -0,0 +1,27 @@
+package com.silverline.task.coursecontent.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String text;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "content_id")
+    private CourseContent courseContent;
+}
